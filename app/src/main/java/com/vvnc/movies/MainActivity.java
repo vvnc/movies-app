95cdda574112278c7539b0470bd0ed2873198dc3
vvnc <vincent.roth@yandex.ru>
2018-01-28 20:15:03 +0300

    Move random icon generation from model to activity
    because these icons are placeholders for empty icons got from server.
    They're taken from activity resources and don't belong to the model.

--- app/src/main/java/com/vvnc/movies/MainActivity.java
+++ app/src/main/java/com/vvnc/movies/MainActivity.java
@@ -1,8 +1,10 @@
 package com.vvnc.movies;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,17 +17,73 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private CustomOnScrollListener onScrollListener;
     private LinearLayoutManager layoutManager;
+    private Random random;
+    private int[] placeholderIcons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        random = new Random();
+        placeholderIcons = new int[]{
+                R.drawable.apple_,
+                R.drawable.bacon_,
+                R.drawable.bananas_,
+                R.drawable.bell_pepper,
+                R.drawable.broccoli_,
+                R.drawable.carrot_,
+                R.drawable.cheese_,
+                R.drawable.cherry_,
+                R.drawable.chicken_leg,
+                R.drawable.chili_pepper,
+                R.drawable.chocolate_,
+                R.drawable.coffee_cup,
+                R.drawable.cookie_,
+                R.drawable.corn_,
+                R.drawable.croissant_,
+                R.drawable.cupcake_,
+                R.drawable.donut_,
+                R.drawable.eggplant_,
+                R.drawable.fortune_cookie,
+                R.drawable.french_fries,
+                R.drawable.fried_egg,
+                R.drawable.grapes_,
+                R.drawable.hamburger_,
+                R.drawable.hot_dog,
+                R.drawable.ice_cream,
+                R.drawable.ketchup_,
+                R.drawable.meatball_,
+                R.drawable.melon_,
+                R.drawable.milk_,
+                R.drawable.milkshake_,
+                R.drawable.mushroom_,
+                R.drawable.mustard_,
+                R.drawable.onigiri_,
+                R.drawable.orange_,
+                R.drawable.pea_,
+                R.drawable.peach_,
+                R.drawable.pear_,
+                R.drawable.piece_of_cake,
+                R.drawable.pineapple_,
+                R.drawable.pizza_,
+                R.drawable.popsicle_,
+                R.drawable.pudding_,
+                R.drawable.radish_,
+                R.drawable.scallion_,
+                R.drawable.soda_,
+                R.drawable.strawberry_,
+                R.drawable.sushi_,
+                R.drawable.taco_,
+                R.drawable.toast_,
+                R.drawable.watermelon_
+        };
+
         recyclerView = findViewById(R.id.movies_recycler_view);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        movies = MovieModel.loadPage(this, 0);
+        movies = MovieModel.loadPage(0, genNextPlaceholderIcon());
         adapter = new MoviesAdapter(movies);
         recyclerView.setAdapter(adapter);
 
@@ -51,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
             Log.d("SLEEP_FAILED", e.toString());
         }
         int insertedPosition = movies.size() + 1;
-        ArrayList<MovieModel> newPortion = MovieModel.loadPage(this, page);
+        ArrayList<MovieModel> newPortion = MovieModel.loadPage(page, genNextPlaceholderIcon());
         movies.addAll(newPortion);
         adapter.notifyItemRangeInserted(insertedPosition, newPortion.size());
     }
@@ -61,5 +119,11 @@ public class MainActivity extends AppCompatActivity {
         int firstVisiblePos = layoutManager.findFirstVisibleItemPosition();
         movies.subList(0, firstVisiblePos - 1).clear();
         adapter.notifyItemRangeRemoved(0, firstVisiblePos);
+    }
+
+    private Drawable genNextPlaceholderIcon() {
+        // Get next random icon from resources:
+        return getResources().getDrawable(
+                placeholderIcons[random.nextInt(placeholderIcons.length)]);
     }
 }
--- app/src/main/java/com/vvnc/movies/MovieModel.java
+++ app/src/main/java/com/vvnc/movies/MovieModel.java
@@ -1,11 +1,8 @@
 package com.vvnc.movies;
-
-import android.content.Context;
-import android.graphics.drawable.Drawable;
-
 import com.vvnc.lorem.Capitalization;
 import com.vvnc.lorem.LoremIpsumGenerator;
 
+import android.graphics.drawable.Drawable;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -17,7 +14,6 @@ class MovieModel {
     private static final int pageItemsCount = 10;
     private static final int wordCountMin = 1;
     private static final int wordCountMax = 15;
-    private static Random random;
 
     private String title;
     private Date dvdDate;
@@ -25,7 +21,6 @@ class MovieModel {
 
     static {
         calendar = Calendar.getInstance();
-        random = new Random();
     }
 
     private MovieModel(String title, Date dvdDate, Drawable poster) {
@@ -46,12 +41,11 @@ class MovieModel {
         return poster;
     }
 
-    static ArrayList<MovieModel> loadPage(Context context, int page){
+    static ArrayList<MovieModel> loadPage(int page, Drawable placeholderIcon){
         // Debug version: randomly generated data:
         ArrayList<MovieModel> data = new ArrayList<>(pageItemsCount);
-        Drawable icon = genNextIcon(context);
         for(int i = 0x00; i < pageItemsCount; ++i) {
-            data.add(new MovieModel(genNextTitle(page, i), genNextDate(), icon));
+            data.add(new MovieModel(genNextTitle(page, i), genNextDate(), placeholderIcon));
         }
         return data;
     }
@@ -70,62 +64,5 @@ class MovieModel {
                 page,
                 itemNum,
                 LoremIpsumGenerator.getNext(wordCount, Capitalization.FIRST_WORD));
-    }
-
-    private static Drawable genNextIcon(Context context){
-        // Get next random icon from resources
-        int[] icons = new int[] {
-            R.drawable.apple_,
-            R.drawable.bacon_,
-            R.drawable.bananas_,
-            R.drawable.bell_pepper,
-            R.drawable.broccoli_,
-            R.drawable.carrot_,
-            R.drawable.cheese_,
-            R.drawable.cherry_,
-            R.drawable.chicken_leg,
-            R.drawable.chili_pepper,
-            R.drawable.chocolate_,
-            R.drawable.coffee_cup,
-            R.drawable.cookie_,
-            R.drawable.corn_,
-            R.drawable.croissant_,
-            R.drawable.cupcake_,
-            R.drawable.donut_,
-            R.drawable.eggplant_,
-            R.drawable.fortune_cookie,
-            R.drawable.french_fries,
-            R.drawable.fried_egg,
-            R.drawable.grapes_,
-            R.drawable.hamburger_,
-            R.drawable.hot_dog,
-            R.drawable.ice_cream,
-            R.drawable.ketchup_,
-            R.drawable.meatball_,
-            R.drawable.melon_,
-            R.drawable.milk_,
-            R.drawable.milkshake_,
-            R.drawable.mushroom_,
-            R.drawable.mustard_,
-            R.drawable.onigiri_,
-            R.drawable.orange_,
-            R.drawable.pea_,
-            R.drawable.peach_,
-            R.drawable.pear_,
-            R.drawable.piece_of_cake,
-            R.drawable.pineapple_,
-            R.drawable.pizza_,
-            R.drawable.popsicle_,
-            R.drawable.pudding_,
-            R.drawable.radish_,
-            R.drawable.scallion_,
-            R.drawable.soda_,
-            R.drawable.strawberry_,
-            R.drawable.sushi_,
-            R.drawable.taco_,
-            R.drawable.toast_,
-            R.drawable.watermelon_
-        };
-        return context.getResources().getDrawable(icons[random.nextInt(icons.length)]);
     }
 }
