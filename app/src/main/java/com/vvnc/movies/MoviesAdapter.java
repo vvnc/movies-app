8d2a8419c3eafd1804cbe03af22ab1e07988d386
vvnc <vincent.roth@yandex.ru>
2018-02-18 17:14:57 +0300

    Add progress wheel when the new items are loaded

--- app/src/main/java/com/vvnc/movies/MoviesAdapter.java
+++ app/src/main/java/com/vvnc/movies/MoviesAdapter.java
@@ -5,28 +5,40 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Locale;
 
-public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder> {
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.BaseViewHolder> {
     private ArrayList<MovieModel> movies;
 
-    // Provide a reference to the views for each data item
-    // Complex data items may need more than one view per item, and
-    // you provide access to all the views for a data item in a view holder
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    static abstract class BaseViewHolder extends RecyclerView.ViewHolder {
+        private BaseViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    private static class ItemViewHolder extends BaseViewHolder {
         // each data item is just a string in this case
         private TextView title;
         private TextView dvdDate;
         private ImageView poster;
-        private ViewHolder(View itemView) {
+        private ItemViewHolder(View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.movie_title);
             dvdDate = itemView.findViewById(R.id.dvd_date);
             poster = itemView.findViewById(R.id.poster);
+        }
+    }
+
+    private static class ProgressViewHolder extends BaseViewHolder {
+        private ProgressBar progressBar;
+        private ProgressViewHolder(View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.progressBar);
         }
     }
 
@@ -35,31 +47,47 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.ViewHolder
         this.movies = movies;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        return position >= movies.size() ? ItemType.PROGRESS.ordinal(): ItemType.MOVIE.ordinal();
+    }
+
     // Create new views (invoked by the layout manager)
     @Override
-    public MoviesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
-                                                       int viewType) {
-        // create a new view
-        View v = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_item, parent, false);
-        // set the view's size, margins, paddings and layout parameters
-        return new ViewHolder(v);
+    public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if(viewType == ItemType.MOVIE.ordinal()) {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_item, parent, false);
+            return new ItemViewHolder(v);
+        } else {
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.progress_item, parent, false);
+            return new ProgressViewHolder(v);
+        }
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
-        // - get element from your dataset at this position
-        // - replace the contents of the view with that element
-        holder.title.setText(movies.get(position).getTitle());
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
-        holder.dvdDate.setText(dateFormat.format(movies.get(position).getDvdDate()));
-        holder.poster.setImageDrawable(movies.get(position).getPoster());
+    public void onBindViewHolder(BaseViewHolder holder, int position) {
+        if (holder instanceof ItemViewHolder) {
+            ItemViewHolder h = (ItemViewHolder) holder;
+            // - get element from your dataset at this position
+            // - replace the contents of the view with that element
+            h.title.setText(movies.get(position).getTitle());
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+            h.dvdDate.setText(dateFormat.format(movies.get(position).getDvdDate()));
+            h.poster.setImageDrawable(movies.get(position).getPoster());
+        } else if (holder instanceof ProgressViewHolder) {
+            ProgressViewHolder h = (ProgressViewHolder) holder;
+            h.progressBar.setIndeterminate(true);
+        } else {
+            throw new MoviesException("Unexpected view holder type in the recycler view: " +
+                holder);
+        }
     }
 
-    // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return movies.size();
+        return movies.size() + 1; // because the extra one is the progress bar
     }
 }
---
+++ app/src/main/java/com/vvnc/movies/MoviesException.java
@@ -0,0 +1,7 @@
+package com.vvnc.movies;
+
+class MoviesException extends RuntimeException {
+    MoviesException(String msg) {
+        super(msg);
+    }
+}
