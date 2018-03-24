80198aea47ddd72af21caa784690ff20f53b0c31
vvnc <vincent.roth@yandex.ru>
2018-03-24 19:28:35 +0300

    Allow 60 loaded items on the screen

--- app/src/main/java/com/vvnc/movies/CustomOnScrollListener.java
+++ app/src/main/java/com/vvnc/movies/CustomOnScrollListener.java
@@ -15,7 +15,7 @@ public abstract class CustomOnScrollListener extends RecyclerView.OnScrollListen
     @Override
     public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
         final int VISIBLE_THRESHOLD = 3;
-        final int MAX_ITEMS_ALLOWED = 30;
+        final int MAX_ITEMS_ALLOWED = 60;
 
         super.onScrolled(recyclerView, dx, dy);
         int totalCount = layoutManager.getItemCount();
